aeb67bb1ce5ce1596b591edef18c588df5ccd6d3
Elhadroubi-Mostafa <elhadroubi.mostafa.solicode@gmail.com>
2024-05-29 11:56:00 +0100

    Add files via upload

---
+++ view/include/nav.php
@@ -0,0 +1,13 @@
+<div class="navbar navbar-expand navbar-light bg-light">
+<div class="nav navbar-nav">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="index.php?action=liste">Listes des stagiaires</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?action=create">Ajouter stagiaire</a>
+      </li>
+    </ul>
+  </div>
+</div>
+
